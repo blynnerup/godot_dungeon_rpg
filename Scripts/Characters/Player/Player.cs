ada24ded5d9c8559f55160d3de5e99adf717010d
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-02-20 14:19:00 +0100

    Implements Player movement

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,19 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 _direction = new();
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = new Vector3(_direction.X, 0, _direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        _direction = Input.GetVector(
+            "MoveLeft", "MoveRight", "MoveForward", "MoveBackwards"
+        );
+    }
 }

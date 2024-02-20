f8c0ae0e88b13a3b3660b2233c490a62012d0f86
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2024-02-20 21:36:44 +0100

    Player changes
    Adds moving and playing animations. Optimizes sprites. Adds constants for Strings

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -1,9 +1,23 @@
+using DungeonRPG.Scripts.General;
 using Godot;
-using System;
+
+namespace DungeonRPG.Scripts.Characters.Player;
 
 public partial class Player : CharacterBody3D
 {
-    private Vector2 _direction = new();
+    [ExportGroup("Required Nodes")]
+    [Export]
+    private AnimationPlayer _animationPlayerNode;
+    [Export]
+    private Sprite3D _playerSprite3D;
+    
+    private Vector2 _direction;
+
+    public override void _Ready()
+    {
+        _animationPlayerNode.Play(GameConstants.AnimIdle);
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new Vector3(_direction.X, 0, _direction.Y);
@@ -14,8 +28,20 @@ public partial class Player : CharacterBody3D
 
     public override void _Input(InputEvent @event)
     {
+        // Get the input from the keyboard
         _direction = Input.GetVector(
-            "MoveLeft", "MoveRight", "MoveForward", "MoveBackwards"
+            GameConstants.InputMoveLeft, GameConstants.InputMoveRight, GameConstants.InputMoveUp, GameConstants.InputMoveDown
         );
+        
+        // Play animation Idle or Move depending on input or no input
+        _animationPlayerNode.Play(_direction == Vector2.Zero ? GameConstants.AnimIdle : GameConstants.AnimMove);
+        
+        // Check move direction in relation to X, flip sprite accordingly
+        _playerSprite3D.FlipH = _direction.X switch
+        {
+            < 0 => true,
+            > 0 => false,
+            _ => _playerSprite3D.FlipH
+        };
     }
 }
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,11 @@
+namespace DungeonRPG.Scripts.General;
+
+public abstract class GameConstants
+{
+    public const string AnimIdle = "Idle";
+    public const string AnimMove = "Move";
+    public const string InputMoveLeft = "MoveLeft";
+    public const string InputMoveUp = "MoveForward";
+    public const string InputMoveDown = "MoveBackward";
+    public const string InputMoveRight = "MoveRight";
+}
